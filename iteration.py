db102befd71a2073d0bb0e47696fa50b54368687
ZorexSalvo <zsalvo@ayannah.com>
2016-07-23 22:15:53 +0800

    Create script for continuous fetching of twitter feeds
    Signed-off-by: ZorexSalvo <zsalvo@ayannah.com>

---
+++ iteration.py
@@ -0,0 +1,13 @@
+from twisted.internet import task
+from twisted.internet import reactor
+import requests
+
+timeout = 150.0 # 150 seconds
+
+def doWork():
+    resp = requests.get('https://2363c32a.ngrok.io/')
+
+l = task.LoopingCall(doWork)
+l.start(timeout) # call every sixty seconds
+
+reactor.run()
